83d6abada6181cf2c43c32e3d1f3745e948a806f
GBValdez <gabrielbenjaminvaldezdeleon@gmail.com>
2023-12-17 17:03:22 -0600

    Se agrego un icono al fondo

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -20,7 +20,7 @@ import {
   Runner,
   World,
 } from 'matter-js';
-import { skillLogos } from './skills';
+import { skillBody, skillLogos } from './skills';
 
 @Component({
   selector: 'app-skills',
@@ -30,7 +30,7 @@ import { skillLogos } from './skills';
   styleUrl: './skills.component.scss',
 })
 export class SkillsComponent implements AfterViewInit {
-  skillsBodies: Body[] = [];
+  skillsBodies: skillBody[] = [];
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
@@ -44,16 +44,16 @@ export class SkillsComponent implements AfterViewInit {
   update(): void {
     Events.on(this.engine, 'beforeUpdate', (event) => {
       this.skillsBodies.forEach((body) => {
-        if (body.position.y > window.innerHeight + 50) {
-          Body.setPosition(body, { x: body.position.x, y: -50 });
+        if (body.body.position.y > window.innerHeight + 50) {
+          Body.setPosition(body.body, { x: body.body.position.x, y: -50 });
         }
-        if (body.position.x > window.innerWidth + 50) {
-          Body.setPosition(body, { x: -50, y: body.position.y });
+        if (body.body.position.x > window.innerWidth + 50) {
+          Body.setPosition(body.body, { x: -50, y: body.body.position.y });
         }
-        if (body.position.x < -50) {
-          Body.setPosition(body, {
+        if (body.body.position.x < -50) {
+          Body.setPosition(body.body, {
             x: window.innerWidth + 50,
-            y: body.position.y,
+            y: body.body.position.y,
           });
         }
       });
@@ -77,33 +77,6 @@ export class SkillsComponent implements AfterViewInit {
   }
 
   addBase(): void {
-    // const base = Bodies.rectangle(
-    //   0,
-    //   window.innerHeight - 10,
-    //   window.innerWidth * 2,
-    //   20,
-    //   {
-    //     isStatic: true,
-    //   }
-    // );
-    // const baseLeft = Bodies.rectangle(0, 0, 5, window.innerHeight * 10, {
-    //   isStatic: true,
-    // });
-    // const baseRight = Bodies.rectangle(
-    //   window.innerWidth,
-    //   0,
-    //   5,
-    //   window.innerHeight * 2,
-    //   {
-    //     isStatic: true,
-    //   }
-    // );
-    // const baseTop = Bodies.rectangle(0, 0, window.innerWidth * 2, 5, {
-    //   isStatic: true,
-    // });
-    // const baseTop = Bodies.rectangle(20, 0, window.innerWidth * 2, 5, {
-    //   isStatic: true,
-    // });
     let base: Body[] = [];
     for (let y = 0; y < 10; y++) {
       for (let x = 0; x < 10; x++) {
@@ -119,35 +92,32 @@ export class SkillsComponent implements AfterViewInit {
 
     const mouse = Mouse.create(this.render.canvas);
     const MOUSE_CONSTRAINTS = MouseConstraint.create(this.engine, { mouse });
-    Composite.add(this.engine.world, [
-      // base,
-      // baseLeft,
-      // baseRight,
-      // baseTop,
-      MOUSE_CONSTRAINTS,
-      ...base,
-    ]);
+    Composite.add(this.engine.world, [MOUSE_CONSTRAINTS, ...base]);
   }
 
   addBox(): void {
     this.skillsBodies = skillLogos.map((logo, index) => {
-      return Bodies.circle(100 + index * 50, 10, 50, {
-        render: {
-          sprite: {
-            texture: `./assets/img/logos/${logo}`,
-            xScale: 2.5,
-            yScale: 2.5,
+      return {
+        body: Bodies.circle(100 + index * 50, 10, 50, {
+          render: {
+            sprite: {
+              texture: `./assets/img/logos/${logo.logo}`,
+              xScale: 2.5,
+              yScale: 2.5,
+            },
+            fillStyle: 'red',
+            strokeStyle: 'blue',
           },
-          fillStyle: 'red',
-          strokeStyle: 'blue',
-        },
-        mass: 12,
-        restitution: 0.7,
-        friction: 1,
-      });
+          mass: 12,
+          restitution: 0.7,
+          friction: 1,
+        }),
+        color: logo.color,
+        logo: logo.logo,
+      };
     });
 
-    Composite.add(this.engine.world, [...this.skillsBodies]);
+    Composite.add(this.engine.world, [...this.skillsBodies.map((b) => b.body)]);
   }
   @ViewChild('view') canvas!: ElementRef<HTMLCanvasElement>;
   engine: Engine = Engine.create();
--- src/app/pages/skills/skills.ts
+++ src/app/pages/skills/skills.ts
@@ -1,17 +1,27 @@
-export const skillLogos: string[] = [
-  'angular.svg',
-  'css.svg',
-  'figma.svg',
-  'git.svg',
-  'gitHub.svg',
-  'godot.svg',
-  'html.svg',
-  'javascript.svg',
-  'mysql.svg',
-  'postgresql.svg',
-  'python.svg',
-  'selenium.svg',
-  'springboot.svg',
-  'tailwind.svg',
-  'typescript.svg',
+import * as matter from 'matter-js';
+
+export interface skillsInfo {
+  logo: string;
+  color: string;
+}
+export interface skillBody extends skillsInfo {
+  body: matter.Body;
+}
+
+export const skillLogos: skillsInfo[] = [
+  { logo: 'angular.svg', color: '#F20000' },
+  { logo: 'css.svg', color: '#1572B6' },
+  { logo: 'figma.svg', color: '#F24E1E' },
+  { logo: 'git.svg', color: '#F05032' },
+  { logo: 'gitHub.svg', color: '#181717' },
+  { logo: 'godot.svg', color: '#478CBF' },
+  { logo: 'html.svg', color: '#E34F26' },
+  { logo: 'javascript.svg', color: '#F7DF1E' },
+  { logo: 'mysql.svg', color: '#4479A1' },
+  { logo: 'postgresql.svg', color: '#4169E1' },
+  { logo: 'python.svg', color: '#3776AB' },
+  { logo: 'selenium.svg', color: '#43B02A' },
+  { logo: 'springboot.svg', color: '#6DB33F' },
+  { logo: 'tailwind.svg', color: '#06B6D4' },
+  { logo: 'typescript.svg', color: '#3178C6' },
 ];
